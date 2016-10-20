2c2cac2891e8376fa31e1d5926c6010c7caaceb3
yanqd1 <yanqd1@lenovo.com>
2016-10-20 18:06:58 +0800

    Add bash in confal.

--- Linux/.bash/config_all.bash
+++ Linux/.bash/config_all.bash
@@ -3,6 +3,19 @@
 
 ### Configurate all configurable commands ###
 
+bash_config () {
+    echo "
+# vim: set shiftwidth=4 softtabstop=-1 expandtab foldmethod=marker:
+# vim: set textwidth=80 colorcolumn=80:
+
+# Personal config {{{
+if [ -f ~/.bash/bashrc ]; then
+    . ~/.bash/bashrc
+fi
+# }}}
+" >> ~/.bashrc
+}
+
 git_config () {
     git config --global core.editor vim
     git config --global push.default simple
@@ -14,14 +27,24 @@ git_config () {
     git config --global alias.lg 'log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
 }
 
-case $1 in
-    git)
-        git_config;;
-    *)
-        echo $1 is not supported yet!
-        echo You can write it in ~/.bash/config_all.bash;;
-esac
+main () {
+    case $1 in
+        bash)
+            bash_config;;
+        git)
+            git_config;;
+        "")
+            echo 'A config name is needed:'
+            echo '  bash'
+            echo '  git'
+            ;;
+        *)
+            echo $1 is not supported yet!
+            echo You can write it in ~/.bash/config_all.bash;;
+    esac
+}
 
+main $*
 
 # vim: set shiftwidth=4 softtabstop=-1 expandtab:
 # vim: set foldmethod=indent foldnestmax=1:
