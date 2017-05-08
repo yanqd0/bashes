8eec5f2e267f0f577aa50e891a5bda7f2eb50086
Yan QiDong <yanqd0@outlook.com>
2017-05-08 17:37:33 +0800

    Restructure init_sys with bash array.

--- init_sys.bash
+++ init_sys.bash
@@ -2,24 +2,34 @@
 
 ### Initialize in a Debian/Ubuntu system ###
 
-# aptitude install build-essential
+packages=(
+    build-essential
 
-aptitude install git
-# Version Control System
+    git
+    # Version Control System
 
-aptitude install silversearcher-ag
-# A code-searching tool similar to ack, but faster.
-# See: https://github.com/ggreer/the_silver_searcher
+    silversearcher-ag
+    # A code-searching tool similar to ack, but faster.
+    # See: https://github.com/ggreer/the_silver_searcher
 
-aptitude install shellcheck
-# A static analysis tool for shell scripts.
-# See: https://github.com/koalaman/shellcheck
+    shellcheck
+    # A static analysis tool for shell scripts.
+    # See: https://github.com/koalaman/shellcheck
 
-aptitude install jq
-# Command-line JSON processor
-# See: https://github.com/stedolan/jq
+    jq
+    # Command-line JSON processor
+    # See: https://github.com/stedolan/jq
 
-aptitude install cloc
-# Count Lines of Code
-# See: https://github.com/AlDanial/cloc
+    cloc
+    # Count Lines of Code
+    # See: https://github.com/AlDanial/cloc
+)
 
+if [ "$(id -u)" != "0" ]
+then # Not root
+   INSTALL="sudo aptitude install"
+else # root
+   INSTALL="aptitude install"
+fi
+
+$INSTALL "${packages[@]}"
