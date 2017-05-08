09b278cc4a293583f913ad8e1c7e890d8bbb9bc5
Yan QiDong <yanqd0@outlook.com>
2017-05-08 17:15:04 +0800

    Add ag, shellcheck, jq, cloc to init_sys.

--- init_sys.bash
+++ init_sys.bash
@@ -2,11 +2,24 @@
 
 ### Initialize in a Debian/Ubuntu system ###
 
-sudo apt-get install \
-    build-essential \
-    # Version Control System
-    git \
-    # A code-searching tool similar to ack, but faster.
-    # See: https://github.com/ggreer/the_silver_searcher
-    silversearcher-ag \
+# aptitude install build-essential
+
+aptitude install git
+# Version Control System
+
+aptitude install silversearcher-ag
+# A code-searching tool similar to ack, but faster.
+# See: https://github.com/ggreer/the_silver_searcher
+
+aptitude install shellcheck
+# A static analysis tool for shell scripts.
+# See: https://github.com/koalaman/shellcheck
+
+aptitude install jq
+# Command-line JSON processor
+# See: https://github.com/stedolan/jq
+
+aptitude install cloc
+# Count Lines of Code
+# See: https://github.com/AlDanial/cloc
 
