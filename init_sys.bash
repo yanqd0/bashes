716d0b01004f7dd5030ffd951ff3ee28a628ff7c
Yan QiDong <yanqd0@outlook.com>
2017-05-09 10:32:52 +0800

    Support Darwin with homebrew.

--- init_sys.bash
+++ init_sys.bash
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-### Initialize in a Debian/Ubuntu system ###
+### Initialize a system of Debian/Ubuntu or Darwin. ###
 
-packages=(
-    build-essential
-
+common=(
     git
     # Version Control System
-
-    silversearcher-ag
-    # A code-searching tool similar to ack, but faster.
-    # See: https://github.com/ggreer/the_silver_searcher
 
     shellcheck
     # A static analysis tool for shell scripts.
@@ -25,11 +19,39 @@ packages=(
     # See: https://github.com/AlDanial/cloc
 )
 
-if [ "$(id -u)" != "0" ]
-then # Not root
-   INSTALL="sudo aptitude install"
-else # root
-   INSTALL="aptitude install"
-fi
+debs=(
+    build-essential
 
+    silversearcher-ag
+    # A code-searching tool similar to ack, but faster.
+    # See: https://github.com/ggreer/the_silver_searcher
+)
+
+brews=(
+    ag
+    # Same as silversearcher-ag
+    # See: https://github.com/ggreer/the_silver_searcher
+)
+
+name=$(uname)
+case ${name} in
+    'Linux')
+        if [ "$(id -u)" == "0" ]
+        then # root
+            INSTALL="aptitude install"
+        else # Not root
+            INSTALL="sudo aptitude install"
+        fi
+
+        packages=(${common[@]} ${debs[@]});;
+    'Darwin')
+        INSTALL="brew install"
+        packages=(${common[@]} ${brews[@]});;
+    *)
+        echo "Unsupported system: ${name}" 1>&2
+        exit 1;;
+esac
+echo "Initialize ${name}..."
+
+echo "$INSTALL" "${packages[@]}"
 $INSTALL "${packages[@]}"
