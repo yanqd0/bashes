1c2cdfac6924381cb8b63897436fdae118f8db43
Yan QiDong <yanqd0@outlook.com>
2017-07-19 10:39:56 +0800

    Add a function docker-clean

--- function.bash
+++ function.bash
@@ -142,5 +142,13 @@ function init_sys {
 }
 # }}}
 
+# docker-clean: Clean docker containers, images and volumes. {{{
+function docker-clean {
+    docker rm $(docker ps -aq) 2> /dev/null
+    docker rmi $(docker images -qf "dangling=true") 2> /dev/null
+    docker volume prune -f 2> /dev/null
+}
+# }}}
+
 # vim: set shiftwidth=4 softtabstop=-1 expandtab foldmethod=marker:
 # vim: set textwidth=80 colorcolumn=80:
