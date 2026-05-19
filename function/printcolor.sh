4a09587a232f74b37db81422be7479aa8d514d58
Yan QiDong <yanqd0@outlook.com>
2026-05-19 14:30:34 +0800

    内联 print_color.bash 到 printcolor 函数
    将 `print_color.bash` 的色表打印逻辑内联到 `function/printcolor.sh`，
    不再通过外部脚本调用，并删除原脚本文件。

--- function/printcolor.sh
+++ function/printcolor.sh
@@ -1,11 +1,21 @@
 # printcolor: Print the supported color of current bash emulator. {{{
 printcolor() {
-    script=~/.bash/print_color.bash
-    if [ -f $script ]
-    then
-        $script "$*"
-    else
-        echo $script not found!
-    fi
+    for STYLE in 0 1 2 3 4 5 6 7
+    do
+        for FG in 30 31 32 33 34 35 36 37
+        do
+            for BG in 40 41 42 43 44 45 46 47
+            do
+                CTRL="\033[${STYLE};${FG};${BG}m"
+                echo -en "${CTRL}"
+                echo -n "${STYLE};${FG};${BG}"
+                echo -en "\033[0m"
+            done
+            echo
+        done
+        echo
+    done
+    echo Style \| Frontground \| Background
+    echo -e "\033[0m"
 }
 # }}}
